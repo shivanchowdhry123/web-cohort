ef45dfe802cc36a55c9a18c1fc640a07f8f92057
Shivan Sarang Chowdhry <chowdhryshivan@gmail.com>
2026-06-05 18:39:23 +0530

    feat: initialize project with basic structure and button color-toggle functionality

--- day-idk/script.js
+++ day-idk/script.js
@@ -1,5 +1,18 @@
-
-function Hi() {
-    console.log("Hi, i'm button and I got clicked!!");
+let button = document.getElementById("mybutton");
+// button.onclick = function(){
+//     alert("Hi, i'm button and I got clicked!!");
+// }
+// console.log("Hi, i'm Pulkit, we are going to deploy a website today")
+// NOW WE'LL GONNA CHANGE THE COLOR OF BUTTON TO RED WHEN IT GET'S CLICKED
+// button.onclick = function(){
+//     button.style.backgroundColor = "red";
+// }
+// Now we'll gonna make a color toggle that will change to blue and red
+button.onclick = function () {
+    if (button.style.backgroundColor == "blue") {
+        button.style.backgroundColor = "red";
+    }
+    else {
+        button.style.backgroundColor = "blue";
+    }
 }
-console.log("Hi, i'm Pulkit, we are going to deploy a website today")
